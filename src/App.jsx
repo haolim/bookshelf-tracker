42d11e2bcc46145b554214ee693ef2b9b79d378e
Hao <haolim84@gmail.com>
2026-09-14 17:21:25 +0800

    initial commit with ai generated css stylings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-shelf">
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
+
+export default App;

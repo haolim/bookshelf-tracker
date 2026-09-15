5a2057859bc8fb360a8cd1011496f9e12e398fd9
Hao <haolim84@gmail.com>
2026-09-15 13:59:03 +0800

    feat: implement BookCard and handles for onFinished and onDelete

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,46 @@
+import styles from "./BookCard.module.css";
+
+const statusStyle = {
+  "to-read": styles.toRead,
+  reading: styles.reading,
+  finished: styles.finished,
+};
+
+export default function BookCard({
+  book,
+  onDelete,
+  onFinished,
+  deletingBookId,
+  isDeleting,
+}) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{book.title} </h3>
+        <div className={styles.author}>Author: {book.author}</div>
+        <div className={`${styles.badge} ${statusStyle[book.status]}`}>
+          Status: {book.status}
+        </div>
+      </div>
+
+      {book.status !== "finished" && (
+        <button
+          type="button"
+          className={styles.finishButton}
+          onClick={() => onFinished(book.id)}
+        >
+          Mark as finished
+        </button>
+      )}
+
+      <button
+        type="button"
+        className={styles.deleteButton}
+        onClick={() => onDelete(book.id)}
+        disabled={isDeleting}
+      >
+        {deletingBookId !== book.id ? "Delete" : "Deleting..."}
+      </button>
+    </div>
+  );
+}
